d8ce45648a31bac6bc4bae8a64c2b2896be214a0
ultraemson <esf12364@gmail.com>
2024-01-06 10:04:44 -0300

    Aula: STATICFILES_DIRS

--- projeto/settings.py
+++ projeto/settings.py
@@ -41,10 +41,6 @@ INSTALLED_APPS = [
     'recipes',
 ]
 # Codigo copiado da documentação
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' 
+
+# Adicionar novos diretorios estaticos, Exemplo a baixo
+STATICFILES_DIRS = [
+    BASE_DIR / 'base_static/',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
