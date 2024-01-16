131385d1d645670538ecee9142c383a5cae7f0da
ultraemson <esf12364@gmail.com>
2024-01-16 16:41:43 -0300

    diferença entre paginas

--- recipes/views.py
+++ recipes/views.py
@@ -3,15 +3,13 @@ from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
-    # NAME ESPECER = informa a pasta e o arquivo que vai ser lido primeiro
     return render(request, 'recipes/pages/index.html', context={
         'recipes' : [make_recipe() for _ in range(10)],
     })
-    #
-    # Vai mostrar  a pagina "base_template/global/idex.html" 
-    # return render(request, 'global/index.html', status=404)
+   
 def recipe(request, id):
-    # NAME ESPECER = informa a pasta e o arquivo que vai ser lido primeiro
+   
     return render(request, 'recipes/pages/recipe-views.html', context={
         'recipe' : make_recipe(),
+        'is_detail_page': True,
     })
