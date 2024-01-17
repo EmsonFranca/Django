6670382277a02cd07d8c876f0ec76d323edd5297
ultraemson <esf12364@gmail.com>
2024-01-17 15:36:38 -0300

    adicionando app_name para namespace da url

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 # Importando o modulo 
 from  recipes import views
-    
+
+app_name = 'recipes'
+  
 urlpatterns = [
     
-    path('', views.home), # Pagina inicial
-    path('recipes/<id>/', views.recipe),
+    path('', views.home, name='home'), # Pagina inicial
+    path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]
