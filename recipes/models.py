d3db02647d0906bebe19525776ccf3b5dfb0aa3e
ultraemson <esf12364@gmail.com>
2024-01-19 10:01:33 -0300

    criando o super usuario e registrando a categoria na admin do Django

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField( max_length = 65 )
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.CharField( max_length = 65 )
