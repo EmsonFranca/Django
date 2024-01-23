13747bacbf29899ae83d9f169ec0af72b3420231
ultraemson <esf12364@gmail.com>
2024-01-23 17:19:59 -0300

    usando o model recipe real com querySet na view home

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,5 +7,6 @@ app_name = 'recipes'
 urlpatterns = [
     
     path('', views.home, name='home'), # Pagina inicial
+    path('recipes/category/<int:category_id>/', views.category, name='category'),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 # Create your views here.
 def home(request):
+    recipe = Recipe.objects.all().order_by('-id')
     return render(request, 'recipes/pages/index.html', context={
-        'recipes' : [make_recipe() for _ in range(10)],
+        'recipes' : recipe,
+    })
+def category(request, category_id):
+    recipe = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/index.html', context={
+        'recipes' : recipe,
     })
    
 def recipe(request, id):
