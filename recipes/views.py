4aa4d5c73a41ba11644fb5b23a61d6c67041e0a7
ultraemson <esf12364@gmail.com>
2024-01-23 20:20:53 -0300

    Ocultando receitas não publicadas

--- recipes/views.py
+++ recipes/views.py
@@ -4,14 +4,16 @@ from .models import Recipe
 
 # Create your views here.
 def home(request):
-    recipe = Recipe.objects.all().order_by('-id')
+    
+    recipes = Recipe.objects.filter(is_published = True).order_by('-id')
     return render(request, 'recipes/pages/index.html', context={
-        'recipes' : recipe,
+        'recipes' : recipes,
     })
+    
 def category(request, category_id):
-    recipe = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/index.html', context={
-        'recipes' : recipe,
+    recipes = Recipe.objects.filter(category__id= category_id, is_published = True ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes' : recipes,
     })
    
 def recipe(request, id):
